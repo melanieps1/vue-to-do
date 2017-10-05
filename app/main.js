c039095e79537dae35cb0daf7095e4766858bd34
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-05 17:00:08 -0400

    initial commit

---
+++ app/main.js
@@ -0,0 +1,33 @@
+var demo = new Vue ({
+
+	el: '#main',
+
+	data: {
+		toDo = ''
+	},
+
+	methods: {
+
+		addTask: function() {
+
+			// Converts what is typed in input box into a new to do at the bottom of the list
+
+		}
+
+		completeTask: function() {
+
+			// Ability to check off to do item with checkbox.  Moves to completed section
+
+		}
+
+		deleteTask: function() {
+
+			// Deletes to do when clicking on X or trash icon.  TBD.
+
+		}
+
+	}
+
+});
+
+// alert("hello!");
